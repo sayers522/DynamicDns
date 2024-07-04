31cb42bd31d2428a30b6348e76f23f1635192025
SayersYang <sayersyang@gmail.com>
2024-07-04 17:49:27 +0000

    add timestamp

--- guardDns.py
+++ guardDns.py
@@ -2,9 +2,11 @@
 # -*- coding: UTF-8 -*-
 from json import load
 from urllib2 import urlopen
+from datetime import datetime
 import BaiduSDK
 
 def diffIp(domain):
+  ts = '[%s] ' % datetime.now()
   #获取本机实时公网IP
   my_ip = urlopen('https://checkip.amazonaws.com/').read().strip()
 
@@ -13,21 +15,21 @@ def diffIp(domain):
 
   #SDK调用失败
   if rdata == "error":
-    print 'Failed to connect Dns service'
+    print ts + 'Failed to connect Dns service'
     return False
   #当前域名没有解析记录，使用当前主机公网IP，创建一条新的解析记录
   elif rdata == "none":
-    print 'No such DNS record, creating a new one'
+    print ts + 'No such DNS record, creating a new one'
     BaiduSDK.addRecord(domain,my_ip)
     return True
   #公网IP发生了变化，和DNS解析记录的IP地址不同，更新解析记录
   elif rdata != my_ip:
-    print "The ip has changed: %s -> %s" % (rdata,my_ip)
+    print ts + "The ip has changed: %s -> %s" % (rdata,my_ip)
     BaiduSDK.updateRecord(domain,my_ip,rid)
     return True
   #公网IP没发生变化，不需要更新DNS
   else:
-    print "Not change: %s" % my_ip
+    print ts + "Not change: %s" % my_ip
     return True
 
 if __name__ == '__main__':
